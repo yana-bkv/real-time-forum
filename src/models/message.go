46518ac09fe468c926e62d5ce649bb3251cd9985
jaboikova <yanis.twink@gmail.com>
2025-05-02 21:45:49 +0300

    fix

--- src/models/message.go
+++ src/models/message.go
@@ -1,1 +1,8 @@
 package models
+
+type Message struct {
+	Sender    string `json:"sender"`
+	Receiver  string `json:"receiver"`
+	Content   string `json:"content"`
+	TimeStamp int    `json:"timestamp"`
+}
